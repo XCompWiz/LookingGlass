659aaafda6558ca3e4322c712779f4bf87c0df38
XCompWiz <xcompwiz@gmail.com>
2018-06-28 22:36:04 +0300

    Update portal render

--- src/main/java/com/xcompwiz/lookingglass/client/render/RenderPortal.java
+++ src/main/java/com/xcompwiz/lookingglass/client/render/RenderPortal.java
@@ -5,15 +5,27 @@ import org.lwjgl.opengl.GL11;
 import com.xcompwiz.lookingglass.api.view.IWorldView;
 import com.xcompwiz.lookingglass.entity.EntityPortal;
 
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderPortal extends Render {
+public class RenderPortal extends Render<EntityPortal> {
+
+	public RenderPortal(RenderManager renderManager) {
+		super(renderManager);
+	}
 
 	@Override
-	public void doRender(Entity entity, double d, double d1, double d2, float f, float f1) {
+	protected ResourceLocation getEntityTexture(EntityPortal entity) {
+		return null;
+	}
+
+	@Override
+	public void doRender(EntityPortal entity, double x, double y, double z, float entityYaw, float partial) {
 		if (!(entity instanceof EntityPortal)) return;
 		EntityPortal portal = (EntityPortal) entity;
 		IWorldView activeview = portal.getActiveView();
@@ -24,46 +36,40 @@ public class RenderPortal extends Render {
 
 		int width = 2;
 		int height = 3;
-		double left = -width / 2.;
-		double top = 0;
 
 		activeview.markDirty();
-		GL11.glDisable(GL11.GL_ALPHA_TEST);
-		GL11.glDisable(GL11.GL_LIGHTING);
+
+		GlStateManager.disableAlpha();
+		GlStateManager.disableLighting();
 
 		GL11.glPushMatrix();
-		GL11.glTranslatef((float) d, (float) d1, (float) d2);
+
+		GlStateManager.translate(x, y, z);
+		GlStateManager.rotate(-entityYaw, 0.0F, 1.0F, 0.0F);
+
+		Tessellator tes = Tessellator.getInstance();
+		BufferBuilder vb = tes.getBuffer();
 
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture);
-		Tessellator tessellator = Tessellator.instance;
-		tessellator.setColorRGBA_F(1, 1, 1, 1);
-		tessellator.startDrawingQuads();
-		tessellator.addVertexWithUV(left, top, 0.0D, 0.0D, 0.0D); //inc=bl out; inc=bl down
-		tessellator.addVertexWithUV(width + left, top, 0.0D, 1.0D, 0.0D); //dc=br out; inc=br down
-		tessellator.addVertexWithUV(width + left, height + top, 0.0D, 1.0D, 1.0D); //dec=tr out; dec=tr up
-		tessellator.addVertexWithUV(left, height + top, 0.0D, 0.0D, 1.0D); //inc=lt out; dec=tl up
-		tessellator.draw();
+		vb.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+		vb.pos(- width/2, 0, 0.01f).tex(0.0D, 1.0D).endVertex();
+		vb.pos(width/2, 0, 0.01f).tex(1.0D, 1.0D).endVertex();
+		vb.pos(width/2, height, 0.01f).tex(1.0D, 0.0D).endVertex();
+		vb.pos(- width/2,  height, 0.01f).tex(0.0D, 0.0D).endVertex();
+		tes.draw();
+
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
 		//XXX: Make the back of the portals a little nicer
-		tessellator.setColorRGBA_F(0, 0, 1, 1);
-		tessellator.startDrawingQuads();
-		tessellator.addVertexWithUV(left, height + top, 0.0D, 0.0D, 1.0D);
-		tessellator.addVertexWithUV(width + left, height + top, 0.0D, 1.0D, 1.0D);
-		tessellator.addVertexWithUV(width + left, top, 0.0D, 1.0D, 0.0D);
-		tessellator.addVertexWithUV(left, top, 0.0D, 0.0D, 0.0D);
-		tessellator.draw();
+		vb.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+		vb.pos(- width/2,  height, -0.01f).tex(0.0D, 1.0D).endVertex();
+		vb.pos(width/2, height, -0.01f).tex(1.0D, 1.0D).endVertex();
+		vb.pos(width/2, 0, -0.01f).tex(1.0D, 0.0D).endVertex();
+		vb.pos(- width/2, 0, -0.01f).tex(0.0D, 0.0D).endVertex();
+		tes.draw();
 		GL11.glPopMatrix();
 
-		GL11.glEnable(GL11.GL_LIGHTING);
-		GL11.glEnable(GL11.GL_ALPHA_TEST);
-	}
-
-	@Override
-	protected void bindEntityTexture(Entity entity) {}
-
-	@Override
-	protected ResourceLocation getEntityTexture(Entity entity) {
-		return null;
+		GlStateManager.enableLighting();
+		GlStateManager.enableAlpha();
 	}
 
 }
