85f475928709613b9d68851cbc8337bc59f1c5fe
XCompWiz <xcompwiz@gmail.com>
2018-06-28 22:35:49 +0300

    Update command base (command helper functions)

--- src/main/java/com/xcompwiz/lookingglass/command/CommandBaseAdv.java
+++ src/main/java/com/xcompwiz/lookingglass/command/CommandBaseAdv.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
+import net.minecraft.command.EntitySelector;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.NumberInvalidException;
 import net.minecraft.command.PlayerNotFoundException;
-import net.minecraft.command.PlayerSelector;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
@@ -18,43 +18,44 @@ import net.minecraft.world.World;
 public abstract class CommandBaseAdv extends CommandBase {
 
 	public void sendToAdmins(ICommandSender agent, String text, Object[] objects) {
-		func_152373_a(agent, this, text, objects);
+		notifyCommandListener(agent, this, text, objects);
 	}
 
-	public static EntityPlayerMP getTargetPlayer(ICommandSender sender, String target) {
-		EntityPlayerMP entityplayermp = PlayerSelector.matchOnePlayer(sender, target);
+	public static EntityPlayerMP getTargetPlayer(MinecraftServer server, ICommandSender sender, String target) throws PlayerNotFoundException, CommandException {
+		EntityPlayerMP entityplayermp = EntitySelector.matchOnePlayer(sender, target);
 
 		if (entityplayermp == null) {
-			entityplayermp = MinecraftServer.getServer().getConfigurationManager().func_152612_a(target);
+			entityplayermp = server.getPlayerList().getPlayerByUsername(target);
 		}
-		if (entityplayermp == null) { throw new PlayerNotFoundException(); }
+		if (entityplayermp == null) { throw new PlayerNotFoundException("commands.generic.player.unspecified"); }
 		return entityplayermp;
 	}
 
-	public static Integer getSenderDimension(ICommandSender sender) {
+	public static Integer getSenderDimension(ICommandSender sender) throws CommandException {
 		World w = sender.getEntityWorld();
 		if (w == null) throw new CommandException("You must specify a dimension to use this command from the commandline");
-		return w.provider.dimensionId;
+		return w.provider.getDimension();
 	}
 
 	/**
 	 * Returns the given ICommandSender as a EntityPlayer or throw an exception.
+	 * @throws CommandException if the tile entity cannot be found
 	 */
-	public static TileEntity getCommandSenderAsTileEntity(ICommandSender sender) {
+	public static TileEntity getCommandSenderAsTileEntity(ICommandSender sender) throws CommandException {
 		try {
 			World world = sender.getEntityWorld();
-			BlockPos coords = sender.getPlayerCoordinates();
-			return world.getTileEntity(coords.getX(), coords.getY(), coords.getZ());
+			BlockPos coords = sender.getPosition();
+			return world.getTileEntity(coords);
 		} catch (Exception e) {
 			throw new CommandException("Could not get tile entity");
 		}
 	}
 
-	public static double handleRelativeNumber(ICommandSender sender, double origin, String arg) {
+	public static double handleRelativeNumber(ICommandSender sender, double origin, String arg) throws NumberInvalidException {
 		return handleRelativeNumber(sender, origin, arg, -30000000, 30000000);
 	}
 
-	public static double handleRelativeNumber(ICommandSender par1ICommandSender, double origin, String arg, int min, int max) {
+	public static double handleRelativeNumber(ICommandSender par1ICommandSender, double origin, String arg, int min, int max) throws NumberInvalidException {
 		boolean relative = arg.startsWith("~");
 		boolean random = arg.startsWith("?");
 		if (random) relative = true;
@@ -67,7 +68,7 @@ public abstract class CommandBaseAdv extends CommandBase {
 				arg = arg.substring(1);
 			}
 
-			double d2 = parseDouble(par1ICommandSender, arg);
+			double d2 = parseDouble(arg);
 			if (random) {
 				Random rand = new Random();
 				d1 += (rand.nextDouble() * 2 - 1) * d2;
@@ -92,13 +93,13 @@ public abstract class CommandBaseAdv extends CommandBase {
 	/**
 	 * Returns the player for a username as an Entity or throws an exception.
 	 */
-	public static Entity parsePlayerByName(String name) throws PlayerNotFoundException {
-		EntityPlayerMP player = MinecraftServer.getServer().getConfigurationManager().func_152612_a(name);
+	public static Entity parsePlayerByName(MinecraftServer server, String name) throws PlayerNotFoundException {
+		EntityPlayerMP player = server.getPlayerList().getPlayerByUsername(name);
 		if (player != null) { return player; }
-		throw new PlayerNotFoundException("lookingglass.commands.generic.player.notfound", new Object[] { name });
+		throw new PlayerNotFoundException("commands.myst.generic.player.notfound", new Object[] { name });
 	}
 
-	public static float parseFloat(ICommandSender par0ICommandSender, String par1Str) {
+	public static float parseFloat(ICommandSender par0ICommandSender, String par1Str) throws NumberInvalidException {
 		try {
 			return Float.parseFloat(par1Str);
 		} catch (NumberFormatException numberformatexception) {
